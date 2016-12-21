9c70f72d317decf3df808c47a7fb7165a8f30191
ryanjiang <ryan.qiyu.jiang@gmail.com>
2016-12-20 20:21:00 -0500

    stuff
    sdf

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app=express();
+
+var mongojs=require('mongojs');
+var db=mongojs('venusUsers',['venusUsers']);
+var bodyParser=require('body-parser');
+var path = require('path');
+
+app.use(express.static(__dirname+'/public'));
+app.use (bodyParser.json());
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+app.listen(5000);
+console.log("venus server running");
+
